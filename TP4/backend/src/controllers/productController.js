2222846d1245b164d581a2eff95ad9ff46512b5f
Gabriel0258 <gkessleralvarez@gmail.com>
2025-12-04 17:08:05 -0300

    Implementado medidas contra SQL injection

--- TP4/backend/src/controllers/productController.js
+++ TP4/backend/src/controllers/productController.js
@@ -1,21 +1,26 @@
 const { db } = require('../config/database');
 
+
+
 // VULNERABLE: SQL Injection
 const getProducts = (req, res) => {
   const { category, search } = req.query;
   
-  // VULNERABLE: Concatenación directa de strings en SQL
+   // VULNERABLE: Concatenación directa de strings en SQL
   let query = 'SELECT * FROM products WHERE 1=1';
-  
+  const params = [];
+
   if (category) {
-    query += ` AND category = '${category}'`;
+    query += ` AND category = ?`;
+    params.push(category)
   }
   
   if (search) {
-    query += ` AND name LIKE '%${search}%'`;
+    query += ` AND name LIKE ?`;
+    params.push(`%${search}%`);
   }
   
-  db.query(query, (err, results) => {
+  db.query(query, params, (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
--- TP4/backend/src/routes/products.js
+++ TP4/backend/src/routes/products.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const { query, validationResult } = require('express-validator');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
+const sqlRegex = /['"=;()#\-*\/]|--|union|select|drop|delete|insert|update|information_schema/i;
+
+const sanitizarQuery = [ 
+    query('category').isAlphanumeric().trim().optional().escape().custom(cat => {
+        if (sqlRegex.test(cat)) {
+            throw new Error('Carácter inválido.');
+        }
+        return true
+    }),
+    query('search').optional().escape().trim().custom(s => {
+        if (sqlRegex.test(s)) {
+            throw new Error('Carácter inválido.');
+        }
+        return true
+    }),
+    (req, res, next) => {
+        const errores = validationResult(req);
+        if (!errores.isEmpty()) {
+            return res.status(200).json([]);
+        }
+        next();
+    }
+];
 // Ruta de productos (vulnerable a SQL injection)
-router.get('/products', productController.getProducts);
+router.get('/products', sanitizarQuery, productController.getProducts);
 
 module.exports = router;
+// if ((category && sqlRegex.test(category)) || (search && sqlRegex.test(search))) {
+//     return res.status(200).json([])
+//   }
